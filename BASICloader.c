21ea23a1d8299528c5825e1ffa604f32ad24721a
Richard Cavell <richardcavell@mail.com>
2019-04-30 15:15:38 +0100

    Update year to 2019 in BASICloader.c

--- BASICloader.c
+++ BASICloader.c
@@ -1,7 +1,7 @@
 /*
       BASICloader.c
 
-      A program by Richard Cavell (c) 2017-2018
+      A program by Richard Cavell (c) 2017-2019
       https://github.com/richardcavell/BASICloader
 
     Coding standards:
