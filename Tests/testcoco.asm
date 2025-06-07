5ba5b0873dab9708582fab43df2943f34f67a87b
Richard Cavell <richardcavell@mail.com>
2025-06-07 10:13:13 +0100

    Add an assembly-language test for the coco target

--- Tests/testcoco.asm
+++ Tests/testcoco.asm
@@ -4,10 +4,24 @@
 *  This is used to test BASICloader.
 *  Assume the least powerful Coco (Coco 1 with 4 kilobytes of RAM, no extended BASIC, no disk drive)
 *
+*  This file is intended to be input to XRoar
+*
 *  June 2025
 
-start	ldx 1024	; Start of text screen
-	lda 'A'
-	sta ,x          ; Put an A at the top left corner
-	clra
+	ORG $3F00
+
+start:	ldx #$6f 	; DEVNUM
+        lda 0           ; 0 means the screen
+        sta ,x
+
+        ldx #msg
+loop:
+	lda ,x+
+        beq end
+	jsr [$A002]     ; CHROUT
+        bra loop
+end:	clra
 	rts
+
+msg:	FCN "TEST succeeded"
+	END
